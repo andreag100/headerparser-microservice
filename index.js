25b4cc80e70b7697499b149a07a0572481a31f03
BenjaminSemah <benjaminsemah@gmail.com>
2022-11-10 11:54:06 +0000

    Refactor code

--- index.js
+++ index.js
@@ -28,8 +28,9 @@ app.get('/api/whoami', (req, res) => {
   
   const ipaddress = req.ip
   const language = req.headers["accept-language"]
+  const software = req.headers["user-agent"]
 
-  res.json({ ipaddress, language })
+  res.json({ ipaddress, language, software })
 })
 
 // listen for requests :)
