69fac67ed80e2759efcc3d2e71031059d5f9840c
Andrea Greco <andreag.email@gmail.com>
2023-05-28 20:35:45 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
-// Import required modules
-const http = require('https');
-const fs = require('fs');
-const pixelmatch = require('pixelmatch');
-
 // Create an HTTP server
 const server = http.createServer((req, res) => {
   // Handle requests
-  if (req.url === '/compare' && req.method === 'GET') {
+  if (req.method === 'GET' && req.url === '/compare') {
     // Load images and perform comparison
-    const img1 = fs.readFileSync('/images/image1/triangles.png');
-    const img2 = fs.readFileSync('/images/image2/triangles.png');
+    const img1 = fs.readFileSync('images/image1/triangles.png');
+    const img2 = fs.readFileSync('images/image2/triangles.png');
     const { width, height } = getImageDimensions(img1); // Replace with your method to get image dimensions
 
     const diff = new Uint8Array(width * height * 4); // Create an array to store diff data
@@ -24,17 +19,3 @@ const server = http.createServer((req, res) => {
     res.end('Not found');
   }
 });
-
-// Start the server
-const listener = app.listen(process.env.PORT || 10000, () => {
-  console.log('App listening on port ' + listener.address().port);
-});
-
-// Helper function to get image dimensions
-const sharp = require('sharp');
-
-// Helper function to get image dimensions
-async function getImageDimensions(imageBuffer) {
-  const metadata = await sharp(imageBuffer).metadata();
-  return { width: metadata.width, height: metadata.height };
-}
