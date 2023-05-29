61cc7f3ee442cf7f802bb219c08aeb27fa1b3b83
Andrea Greco <andreag.email@gmail.com>
2023-05-29 12:12:01 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,76 +1,72 @@
-// Import required modules
 const http = require('http');
 const fs = require('fs');
 const { createCanvas, loadImage } = require('canvas');
 const pixelmatch = require('pixelmatch');
 const sharp = require('sharp');
+const chokidar = require('chokidar');
+
+// Set up file system watcher for the image folders
+const imageWatcher = chokidar.watch(['image1/1triangles.png', 'image2/2triangles.png'], {
+  ignored: /^\./, // Ignore hidden files
+  persistent: true,
+});
 
 // Create an HTTP server
 const server = http.createServer((req, res) => {
   // Handle requests
   if (req.method === 'GET' && req.url === '/compare') {
-    // Load images and perform comparison
+    // Compare images
+    async function compareImages(imagePath1, imagePath2) {
+      // Load images
+      const img1 = fs.readFileSync(imagePath1);
+      const img2 = fs.readFileSync(imagePath2);
+
+      // Get image dimensions
+      const img1Dimensions = await getImageDimensions(img1);
+      const img2Dimensions = await getImageDimensions(img2);
+
+      if (
+        img1Dimensions.width !== img2Dimensions.width ||
+        img1Dimensions.height !== img2Dimensions.height
+      ) {
+        throw new Error('Image sizes do not match.');
+      }
+
+      const { width, height } = img1Dimensions;
+
+      // Create canvas for comparison
+      const diffCanvas = createCanvas(width, height);
+      const diffContext = diffCanvas.getContext('2d');
+
+      // Compare images and get the diff pixels count
+      const numDiffPixels = pixelmatch(
+        img1, // Reference image
+        img2, // Modified image
+        diffContext.getImageData(0, 0, width, height).data, // Diff canvas data
+        width,
+        height,
+        { threshold: 0.1 }
+      );
+
+      // Do something with the comparison result
+      return { numDiffPixels };
+    }
+
+    // Helper function to get image dimensions
+    async function getImageDimensions(imageBuffer) {
+      const metadata = await sharp(imageBuffer).metadata();
+      return { width: metadata.width, height: metadata.height };
+    }
+
+    // Example usage
     const imagePath1 = 'image1/1triangles.png';
     const imagePath2 = 'image2/2triangles.png';
 
-    Promise.all([loadImage(imagePath1), loadImage(imagePath2)])
-      .then(([img1, img2]) => {
-        const img1Dimensions = { width: img1.width, height: img1.height };
-        const img2Dimensions = { width: img2.width, height: img2.height };
-
-        console.log('Image 1 dimensions:', img1Dimensions);
-        console.log('Image 2 dimensions:', img2Dimensions);
-
-        if (img1Dimensions.width !== img2Dimensions.width || img1Dimensions.height !== img2Dimensions.height) {
-          throw new Error('Image sizes do not match.');
-        }
-
-        const img1Data = getImageData(img1);
-        const img2Data = getImageData(img2);
-
-        return compareImages(img1Data, img2Data, img1Dimensions);
-      })
+    compareImages(imagePath1, imagePath2)
       .then(({ numDiffPixels }) => {
         // Do something with the comparison result
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify({ numDiffPixels }));
       })
       .catch(error => {
-        console.error('Error:', error);
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Internal Server Error');
-      });
-  } else {
-    // Handle other requests
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not found');
-  }
-});
-
-// Start the server
-const port = process.env.PORT || 80; // Use the default HTTP port 80
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
-// Compare images
-function compareImages(img1Data, img2Data, img1Dimensions) {
-  const { width, height } = img1Dimensions;
-
-  console.log('Compare images:', img1Data, img2Data);
-  console.log('Image dimensions:', width, height);
-
-  const diff = new Uint8Array(width * height * 4);
-  const numDiffPixels = pixelmatch(img1Data, img2Data, diff, width, height, { threshold: 0.1 });
-
-  // Do something with the comparison result
-  return { numDiffPixels };
-}
-
-// Get image data
-function getImageData(image) {
-  const canvas = createCanvas(image.width, image.height);
-  const context = canvas.getContext('2d');
-  context.drawImage(image, 0, 0);
-  return context.getImageData(0, 0, image.width, image.height).data;
-}
+        console
