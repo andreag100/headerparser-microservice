a9b8aecaa5d511de97961c7b7ca8d45f0d6c26bd
Andrea Greco <andreag.email@gmail.com>
2023-05-29 12:20:45 +0700

    Update server.js

--- server.js
+++ server.js
@@ -63,10 +63,14 @@ const server = http.createServer((req, res) => {
     const imagePath2 = 'image2/2triangles.png';
 
     compareImages(imagePath1, imagePath2)
-      .then(({ numDiffPixels }) => {
-        // Do something with the comparison result
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ numDiffPixels }));
-      })
-      .catch(error => {
-        console
+  .then(({ numDiffPixels }) => {
+    // Do something with the comparison result
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ numDiffPixels }));
+  })
+  .catch(error => {
+    // Handle the error
+    console.error(error);
+    res.writeHead(500, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ error: 'Internal Server Error' }));
+  });
